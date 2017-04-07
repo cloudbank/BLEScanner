0822d9b8dd64426081c67bcdd75e7a769af96eb5
SG Vogel <sabine.g.vogel@gmail.com>
2017-04-07 14:57:16 -0700

    fix typos

--- Application/src/main/java/cloudbank/ble/model/BLEDevice.java
+++ Application/src/main/java/cloudbank/ble/model/BLEDevice.java
@@ -16,19 +16,21 @@ public class BLEDevice {
     public BLEDevice(String name, int rssi, byte[] scanRecord) {
         this.name = name;
         this.rssi = rssi;
-        this.scanRecord = BLEDevice.byteArrayToString(scanRecord);
+        this.scanRecord = BLEDevice.bytesToHex(scanRecord);
     }
 
 
 
 
-    public static String byteArrayToString(byte[] ba)
-    {
-        StringBuilder hex = new StringBuilder(ba.length * 2);
-        for (byte b : ba)
-            hex.append(b).append(" ");
-
-        return hex.toString();
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;  //byte to into
+            hexChars[j * 2] = hexArray[v >>> 4];  //high nibble
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F]; //low nibble
+        }
+        return new String(hexChars);
     }
 }
 
